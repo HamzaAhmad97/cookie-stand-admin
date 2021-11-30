db4e7b51d10bea2ea291e83fa17734ead1859a77
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-30 19:59:32 +0200

    feat: no cookie stands available will get shown if no cookie stands are availble

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -2,28 +2,42 @@ import { hours } from "../public/data";
 import LocationsData from "./LocationsData";
 export default function ReportTable(props) {
   return (
-    <table className="w-7/12 m-auto mt-8 table-auto">
-      <tr>
-        <th className="w-2/12 bg-green-500 border border-none">Location</th>
-        {hours.map((itm, idx) => (
-          <th key={idx} className="bg-green-500 border-none">
-            {itm}
-          </th>
-        ))}
-        <th className="bg-green-500 border-none">Totals</th>
-      </tr>
+    <>
+      {props.cookieStnds.length ? (
+        <table className="w-7/12 m-auto mt-8 table-auto">
+          <tr>
+            <th className="w-2/12 bg-green-500 border border-none">Location</th>
+            {hours.map((itm, idx) => (
+              <th key={idx} className="bg-green-500 border-none">
+                {itm}
+              </th>
+            ))}
+            <th className="bg-green-500 border-none">Totals</th>
+          </tr>
 
-      <LocationsData {...props}/>
-      
-      <tr>
-        <th className="bg-green-500 border-none">Totals</th>
-        {new Array(14).fill(0).map((_, idx) => props.cookieStnds.map(item => item.hourly_sales).map(val => val[idx]).reduce((tot, elem) => tot + elem, 0)).map((itm, idx) => (
-          <th key={idx} className="bg-green-500 border-none">
-            {itm}
-          </th>
-        ))}
-        <th className="bg-green-500 border-none">Totals</th>
-      </tr>
-    </table>
+          <LocationsData {...props} />
+
+          <tr>
+            <th className="bg-green-500 border-none">Totals</th>
+            {new Array(14)
+              .fill(0)
+              .map((_, idx) =>
+                props.cookieStnds
+                  .map((item) => item.hourly_sales)
+                  .map((val) => val[idx])
+                  .reduce((tot, elem) => tot + elem, 0)
+              )
+              .map((itm, idx) => (
+                <th key={idx} className="bg-green-500 border-none">
+                  {itm}
+                </th>
+              ))}
+            <th className="bg-green-500 border-none">Totals</th>
+          </tr>
+        </table>
+      ) : (
+        <h2 className="mt-12 text-3xl text-center">No Cookie Stands Available</h2>
+      )}
+    </>
   );
 }
